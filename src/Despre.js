a9cd7a85a95c81b3ed277f81da04f251fb788d97
Buzila Ionut <ionutbuzila99@yahoo.com>
2024-03-19 21:57:22 +0200

    seconjd

--- src/Despre.js
+++ src/Despre.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import tineri from "./Fundal.jpeg";
 
 function Despre() {
   return (
     <div className='App'>
-      <h2>Pagina in mentenanta!</h2>
+      <h2 id='titlu'>Tineret Betel Rebrișoara</h2>
+      <h4 id='verset'>„V-am scris, tinerilor, fiindcă sunteţi tari, şi Cuvântul lui Dumnezeu rămâne în voi, şi aţi biruit pe cel rău.” 1 Ioan 2:14.</h4>
+      <img src={tineri} alt="tineri" />
     </div>
   );
 }
--- src/Maps.js
+++ src/Maps.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Maps() {
   return (
-    <div className='App'>
-      <h2>Pagina in mentenanta!</h2>
+    <div className="App">
+      <h1 className="mapsH1">
+        Va asteptam cu drag!
+      </h1>
+      <h1 className="smileyFace">☺</h1>
+      <iframe
+        className="maps"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1563.4293233228395!2d24.451799346734337!3d47.28678222292325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4749fcb7fa18d147%3A0xa8b140307b13d7d!2sBiserica%20Penticostal%C4%83%20%22Betel%22%20Rebri%C8%99oara!5e0!3m2!1sro!2sro!4v1710873040916!5m2!1sro!2sro"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
